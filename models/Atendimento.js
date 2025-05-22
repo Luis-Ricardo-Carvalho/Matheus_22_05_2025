e0c4ce8b3e45f423c8914fe6dff6e226c59f608e
Luis-Ricardo-Carvalho <luisricardocarvalholin@gmail.com>
2025-05-22 10:53:59 -0300

    Add files via upload

---
+++ models/Atendimento.js
@@ -0,0 +1,12 @@
+class Atendimento{
+    nome;
+    cpf;
+    data;
+    hora;
+    dataNascimento;
+
+    toString(){
+        return this.nome + " | " + this.cpf + " | " + this.dataNascimento +" | " + this.data + " | " + this.hora ;
+    }
+
+}
---
+++ models/FilaEncadeada.js
@@ -0,0 +1,66 @@
+//models/FilaEncadeada.js
+
+class FilaEncadeada{
+    #inicio;
+    #fim;
+    #qtd;
+
+    constructor(){
+        this.#inicio = null;
+        this.#fim = null;
+        this.#qtd = 0;
+    }
+
+    isEmpty(){
+        return this.#inicio == null;
+    }
+
+    enqueue(dado){
+        const novoNo = new No(dado);
+        if(!this.isEmpty())
+         this.#fim.proximo = novoNo;
+        else
+           this.#inicio = novoNo; 
+        this.#fim = novoNo;
+        this.#qtd++;
+    }
+
+    dequeue(){
+        if(!this.isEmpty()){
+            const dado = this.#inicio.dado;
+            this.#inicio = this.#inicio.proximo;
+            if(this.#inicio==null)
+                this.#fim = null;
+            this.#qtd--;
+            return dado;
+        }
+        return null;
+    }
+
+    toString(){
+        let fila = "";
+        let aux = this.#inicio;
+        while(aux!=null){
+            fila += aux.dado + "|";
+            aux = aux.proximo;
+        }// fim while
+        return fila;
+   }
+   
+   [Symbol.iterator]() {
+    let aux = this.#inicio;
+    return {
+        next: () => {
+            if (aux!=null) {
+                let dado = aux.dado;
+                aux = aux.proximo;
+                return { value: dado, done: false };
+            } else {
+                return { done: true };
+            }
+        }
+    };
+}
+
+
+}
